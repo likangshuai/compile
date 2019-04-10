52677d5edf9e04e9c782e3262a10f9d525591361
lks <lks@qq.com>
2019-04-10 19:30:02 +0800

    first commit

---
+++ src/com/zzuli/test/junitTest.java
@@ -0,0 +1,31 @@
+package com.zzuli.test;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.zzuli.analysis.Main;
+
+
+public class junitTest {
+	Main m=new Main();
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void test() throws IOException {
+		//读取文件
+		List<String>strList= m.readTestFile("D:/1.txt");
+		String [] list=null;
+		for(int i=0;i<strList.size();i++){
+			System.out.println("进行第"+(i+1)+"行的判断：");
+			list=m.Divide(strList.get(i));	
+			m.Sort(list);
+		}
+	}
+
+}
